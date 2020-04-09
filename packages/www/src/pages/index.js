a15ab49ef5bc0eb7fb4eb01311a204596877cfcf
Szabolcs Nagy <snagy@wcl.american.edu>
2020-04-08 20:59:28 -0400

    Added authentication

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,17 +1,27 @@
 import React from "react";
 import { Container, Heading, Button, Flex } from "theme-ui";
-export default (props) => (
-  <Container>
-    <Flex sx={{ flexDirection: "column", padding: 3 }}>
-      <Heading>Get Stuff Done</Heading>
-      <Button
-        sx={{ marginTop: 2 }}
-        onClick={() => {
-          alert("clicked");
-        }}
-      >
-        Log In
-      </Button>
-    </Flex>
-  </Container>
-);
+import netlifyIdentity from "netlify-identity-widget";
+
+export default (props) => {
+  // useEffect only runs on the browser and not during gatsby build
+  React.useEffect(() => {
+    // needs to run on the client side
+    netlifyIdentity.init({});
+  }, []); // run it once
+
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading>Get Stuff Done</Heading>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            netlifyIdentity.open();
+          }}
+        >
+          Log In
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
